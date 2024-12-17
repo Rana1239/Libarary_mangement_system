52d8399340178fbc9561ffc122e41ffcbfb8dfdb
Rana1239 <112718385+Rana1239@users.noreply.github.com>
2024-12-17 13:24:06 +0200

    Delete Books.py

--- Books.py
+++
@@ -1,9 +0,0 @@
-def create_book( book_title, book_author, book_gener, book_loc, book_status=True, book_id=None):
-    return {
-        'book_id': book_id,
-        'book_title': book_title,
-        'book_author': book_author,
-        'book_gener': book_gener,
-        'book_loc': book_loc,
-        'book_status': book_status
-    }
